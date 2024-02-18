5c695b474b632f00a8b9967384718e185a42fda2
Kek <prevost.kevin.qc@gmail.com>
2024-02-18 04:07:48 +0100

    Ajout du navigateur pour les projet et des actions de ce dernier

--- public/script.js
+++ public/script.js
@@ -1,132 +1,104 @@
-const elementDraggable = document.getElementById('kp_iframe--container');
+const elementDraggable = document.querySelector('.kp_window--container');
+const elementChangeDraggable = document.querySelector('.kp_window--title-zone');
 const elementFullScreen = document.querySelector('.kp_animation_full-screen');
-let isDragging = false;
-let isFullScreen = false;
+const resizeHandle = document.querySelector('.resize-handle');
+let isDragging = false, isFullScreen = false, isResizing = false;
+let offsetX, offsetY, startX, startY, startWidth, startHeight;
 let initialState = { width: '', height: '', left: '', top: '' };
-
-// Initialiser l'état initial dès le chargement ou la création de elementDraggable
-initialState.width = elementDraggable.offsetWidth + 'px';
-initialState.height = elementDraggable.offsetHeight + 'px';
-initialState.left = elementDraggable.style.left;
-initialState.top = elementDraggable.style.top;
-
-elementDraggable.addEventListener('mousedown', function(e) {
-  if (!isFullScreen) {
+const updateInitialState = () => {
+  initialState.width = elementDraggable.offsetWidth + 'px';
+  initialState.height = elementDraggable.offsetHeight + 'px';
+  initialState.left = elementDraggable.style.left;
+  initialState.top = elementDraggable.style.top;
+};
+const addClass = (element, className) => {
+  element.classList.add(className);
+};
+const removeClass = (element, className) => {
+  element.classList.remove(className);
+};
+const handleMouseDownDrag = (e) => {
+  addClass(elementChangeDraggable, 'kp_iframe--container_movement');
+  document.body.style.cursor = 'grabbing';
+  if (!isFullScreen && !isResizing) {
     isDragging = true;
     const rect = elementDraggable.getBoundingClientRect();
     offsetX = e.clientX - rect.left;
     offsetY = e.clientY - rect.top;
   }
-
-  // Vérifier si la fenêtre est au top de la page au début du drag
-  if (elementDraggable.getBoundingClientRect().top <= 0) {
-    elementDraggable.classList.add('kp_iframe--container_full'); // Ajouter la classe
-    elementFullScreen.classList.add('kp_window_isfullscreen'); // Ajouter la classe
-  }
-});
-
-document.addEventListener('mousemove', function(e) {
+};
+const handleMouseMoveDrag = (e) => {
   if (isDragging) {
+    const maxTop = 0;
     const newLeft = e.clientX - offsetX;
     const newTop = e.clientY - offsetY;
+    if (newTop < maxTop) {
+      elementDraggable.style.top = maxTop + 'px';
+    }
     elementDraggable.style.left = newLeft + 'px';
     elementDraggable.style.top = newTop + 'px';
+    if (newTop <= 0) {
+      addClass(elementDraggable, 'kp_iframe--container_full');
+      addClass(elementFullScreen, 'kp_window_isfullscreen');
+    } else {
+      removeClass(elementDraggable, 'kp_iframe--container_full');
+      removeClass(elementFullScreen, 'kp_window_isfullscreen');
+    }
   }
-  // Vérifier si la fenêtre est au top de la page pendant le drag
-  if (isDragging && elementDraggable.getBoundingClientRect().top <= 0) {
-    elementFullScreen.classList.add('kp_window_isfullscreen'); // Ajouter la classe
-  } else {
-    elementFullScreen.classList.remove('kp_window_isfullscreen'); // Retirer la classe
-    elementDraggable.classList.remove('kp_iframe--container_full'); // Retirer la classe
-  }
-});
-
-document.addEventListener('mouseup', function(e) {
+};
+const handleMouseUpDrag = () => {
   isDragging = false;
-
-  // Vérifier si la fenêtre est en haut de la page
-  if (!isFullScreen && elementDraggable.getBoundingClientRect().top <= 0) {
-    isFullScreen = true;
-  } else if (isFullScreen) {
-    // Restaurer les valeurs initiales si la fenêtre est déjà en plein écran
-    elementDraggable.style.width = initialState.width;
-    elementDraggable.style.height = initialState.height;
-    elementDraggable.style.left = initialState.left;
-    elementDraggable.style.top = initialState.top;
-    isFullScreen = false;
+  removeClass(elementChangeDraggable, 'kp_iframe--container_movement');
+  document.body.style.cursor = 'default';
+};
+const handleMouseDownResize = (e) => {
+  e.preventDefault();
+  isResizing = true;
+  startX = e.clientX;
+  startY = e.clientY;
+  startWidth = elementDraggable.offsetWidth;
+  startHeight = elementDraggable.offsetHeight;
+  addClass(resizeHandle, 'kp_while_resizing');
+};
+const handleMouseMoveResize = (e) => {
+  if (isResizing) {
+    const deltaX = e.clientX - startX;
+    const deltaY = e.clientY - startY;
+    const newWidth = startWidth + deltaX;
+    const newHeight = startHeight + deltaY;
+    elementDraggable.style.width = `${newWidth - 10}px`;
+    elementDraggable.style.height = `${newHeight - 50}px`;
   }
-  if (isFullScreen) {
-    elementDraggable.classList.add('kp_iframe--container_full'); // Ajouter la classe
-  }
-  else{
-    elementDraggable.classList.remove('kp_iframe--container_full'); // Retirer la classe
-  }
-});
+};
+const handleMouseUpResize = () => {
+  isResizing = false;
+  removeClass(resizeHandle, 'kp_while_resizing');
+};
+elementChangeDraggable.addEventListener('mousedown', handleMouseDownDrag);
+document.addEventListener('mousemove', handleMouseMoveDrag);
+document.addEventListener('mouseup', handleMouseUpDrag);
 
-window.addEventListener('resize', function() {
-  // Si la fenêtre est en mode plein écran et qu'elle est redimensionnée, marquez-la comme étant en cours de redimensionnement
-  if (document.fullscreenElement && !isResizing) {
-    isResizing = true;
-    setTimeout(function() {
-      isResizing = false;
-    }, 200);
-  }
-});
+resizeHandle.addEventListener('mousedown', handleMouseDownResize);
+document.addEventListener('mousemove', handleMouseMoveResize);
+document.addEventListener('mouseup', handleMouseUpResize);
 
-document.querySelector('.kp_full_screen').addEventListener('click', function() {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen().then(function() {
-      isFullScreen = true;
-    }).catch(function(err) {
-      console.error('Erreur lors de la bascule en mode plein écran:', err);
-    });
-  } else {
-    document.exitFullscreen().then(function() {
-      isFullScreen = false;
-    }).catch(function(err) {
-      console.error('Erreur lors de la sortie du mode plein écran:', err);
-    });
-  }
-});
+updateInitialState();
 
-document.querySelector('.kp_full_screen--folder').addEventListener('click', function() {
-  if (!isFullScreen) {
-    // Sauvegarde des valeurs initiales
-    initialState.width = elementDraggable.offsetWidth -7 + 'px';
-    initialState.height = elementDraggable.offsetHeight -7 + 'px';
-    initialState.left = elementDraggable.style.left;
-    initialState.top = elementDraggable.style.top;
-
-    // Mise en plein écran
-    isFullScreen = true;
-  } else {
-    // Restauration des valeurs initiales
-    elementDraggable.style.width = initialState.width;
-    elementDraggable.style.height = initialState.height;
-    elementDraggable.style.left = initialState.left;
-    elementDraggable.style.top = initialState.top;
-    isFullScreen = false;
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  var hauteurProjet = document.querySelector('.kp_un-projet').offsetHeight;
-  var elementIframe = document.querySelector('.kp_iframe--projet');
-  elementIframe.style.height = 'calc(100% - ' + hauteurProjet + 'px)';
-});
-
-// Double clic sur une fenêtre
 let clicCount = 0;
 let clicTimer;
-function onDoubleClick() {
-    console.log("Double clic détecté !");
+function onDoubleClick() { 
+    const classeRecherchee = 'kp_iframe--container_full';
+    if (elementDraggable.classList.contains(classeRecherchee)) {
+      removeClass(elementDraggable, 'kp_iframe--container_full');
+    } else {
+      addClass(elementDraggable, 'kp_iframe--container_full');
+    }
     clicCount = 0;
 }
 function onSingleClick() {
 }
 function handleClic() {
     clicCount++;
-
     if (clicCount === 1) {
         clicTimer = setTimeout(function() {
             clicCount = 0;
@@ -139,3 +111,47 @@ function handleClic() {
     }
 }
 document.querySelector(".kp_window--title-zone").addEventListener("click", handleClic);
+
+function clickResizeWindow(){
+  const classeRecherchee = 'kp_iframe--container_full';
+  if (elementDraggable.classList.contains(classeRecherchee)) {
+    removeClass(elementDraggable, 'kp_iframe--container_full');
+  } else {
+    addClass(elementDraggable, 'kp_iframe--container_full');
+  }
+}
+document.querySelector(".kp_icon_zone--resize").addEventListener("click", clickResizeWindow);
+
+
+// document.querySelector('.kp_full_screen').addEventListener('click', function() {
+//   if (!document.fullscreenElement) {
+//     document.documentElement.requestFullscreen().then(function() {
+//       isFullScreen = true;
+//     }).catch(function(err) {
+//       console.error('Erreur lors de la bascule en mode plein écran:', err);
+//     });
+//   } else {
+//     document.exitFullscreen().then(function() {
+//       isFullScreen = false;
+//     }).catch(function(err) {
+//       console.error('Erreur lors de la sortie du mode plein écran:', err);
+//     });
+//   }
+// });
+
+function ouvrirPojet(){
+  addClass(elementDraggable, 'kp_iframe--show');
+}
+document.querySelector(".kp_folder--projets").addEventListener("click", ouvrirPojet);
+
+
+function fermerPojet(){
+  removeClass(elementDraggable, 'kp_iframe--show');
+}
+document.querySelector(".kp_icon_zone--close").addEventListener("click", fermerPojet);
+
+
+function fermerDescriptionProjet(){
+  addClass(document.querySelector(".kp_un-projet"), 'kp_un-projet--hide');
+}
+document.querySelector(".kp_un-projet--close").addEventListener("click", fermerDescriptionProjet);
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,18 @@
     </header>
     <main class="kp_main">
         <div class="kp_desktop">
+
+            <!-- Window Internet -->
             <div class="kp_animation_full-screen"></div>
-            <!-- Boutons pour changer de projet -->
             <div class="kp_window--container" id="kp_iframe--container">
                 <div class="kp_window--title-zone">
                     <div class="kp_window--title-zon--title">
                         <p class="kp_p">{{ projetActuel.titre }} - {{ projetActuel.compagnie }}</p>
                     </div>
-                    <div>
-                        <div class="kp_full_screen--folder">O</div>
-                        <div></div>
+                    <div class="kp_full--zone">
+                        <div class="kp_icon_zone  kp_icon_zone--reduct"><img class="kp_icon_zone--img" src="/images/reduct_icn.svg" alt="reduct"></div>
+                        <div class="kp_icon_zone  kp_icon_zone--resize"><img class="kp_icon_zone--img" src="/images/resize_icn.svg" alt="resize"></div>
+                        <div class="kp_icon_zone  kp_icon_zone--close" ><img class="kp_icon_zone--img" src="/images/close_icn.svg" alt="close"></div>
                     </div>
                 </div>
                 <div class="kp_window--border">
@@ -21,28 +23,53 @@
                             {{ projet.titre }}
                         </button>
                         <div class="kp_internet--favorites">
-
                         </div>
+                        
                     </div> 
                     <iframe :src="projetActuel.url" width="600" height="400" frameborder="0" class="kp_iframe--projet"></iframe>
                     
                     <article class="kp_un-projet" v-if="projetActuel">
-                        <h1 class="kp_h1">{{ projetActuel.titre }}</h1>
                         
-                        <p class="kp_p">{{ projetActuel.description }}</p>
+                        <div class="kp_icon_zone  kp_un-projet--close" ><img class="kp_icon_zone--img" src="/images/close_icn.svg" alt="close"></div>
+                        <h1 class="kp_h1">{{ projetActuel.titre }}</h1>
+                        <p class="kp_projet-annee">{{ projetActuel.annee }}</p>
+                        
+                        <p class="kp_p  kp_projet_description">{{ projetActuel.description }}</p>
                         <ul class="kp_projet-technos">
                             <li v-for="technologie in projetActuel.techno" :key="technologie"
                                 :class="['kp_projet-technos', `kp_projet-technos--${technologie.toLowerCase()}`]">
                                 {{ technologie }}
                             </li>
                         </ul>
-                        <p class="kp_projet-annee">{{ projetActuel.annee }}</p>
                     </article>
                 </div>
+                
+                <div class="resize-handle"></div>
+            </div>
+            <!-- Window Internet -->
 
+            <div class="kp_folder--un-ico  kp_folder--projets">
+                <div class="kp_folder--un-ico-container-img">
+                    <img class="kp_folder--img" src="/images/folder.png" alt="close">
+                </div>
+                <p class="kp_folder--un-ico-container-text">
+                    Mes projets Cybertek
+                </p>
             </div>
         </div>
 
+        <div class="kp_notification__zone">
+            <div class="kp_notification--titre">
+                <p class="kp_notification--titre-p">
+                    <span class="kp_notification--titre-span">
+                        Démarrer
+                    </span>
+                </p>
+                <div>
+                    <img class="kp_menu_start--img" src="/images/logo-2024.png" alt="">
+                </div>
+            </div>
+        </div>
         <div class="kp_full_screen">passer en plein écran</div>
     </main>
   </template>
