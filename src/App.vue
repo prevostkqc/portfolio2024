a3b3e2b109c1bc7a6bae55b015762cf20dc66189
Kek <prevost.kevin.qc@gmail.com>
2024-04-30 05:15:54 +0200

    Ajout index

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
     document.title = "Kévin Prévost - Développeur Front-end";
     const meta = document.createElement('meta');
     meta.name = 'robots';
-    meta.content = 'noindex, nofollow';
+    // meta.content = 'noindex, nofollow';
     document.head.appendChild(meta);
   }
 }
